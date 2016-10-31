65ae3001e07e6d3ccc7a34e6131c133800e0a17e
Sharpy <sharpysharpymike@aol.com>
2016-10-31 20:40:48 +0000

    initial commit
    initial commit

--- supergameboy/supergameboy.cpp
+++ supergameboy/supergameboy.cpp
@@ -1,9 +1,9 @@
 #include "supergameboy.hpp"
 
 #ifdef _WIN32
-  #define bsnesexport __declspec(dllexport)
+  #define dllexport __declspec(dllexport)
 #else
-  #define bsnesexport
+  #define dllexport
 #endif
 
 #include <stdlib.h>
@@ -12,57 +12,57 @@
 
 #include <interface/interface.cpp>
 
-bsnesexport void sgb_rom(uint8_t *data, unsigned size) {
+dllexport void sgb_rom(uint8_t *data, unsigned size) {
   supergameboy.romdata = data;
   supergameboy.romsize = size;
 }
 
-bsnesexport void sgb_ram(uint8_t *data, unsigned size) {
+dllexport void sgb_ram(uint8_t *data, unsigned size) {
   supergameboy.ramdata = data;
   supergameboy.ramsize = size;
 }
 
-bsnesexport void sgb_rtc(uint8_t *data, unsigned size) {
+dllexport void sgb_rtc(uint8_t *data, unsigned size) {
   supergameboy.rtcdata = data;
   supergameboy.rtcsize = size;
 }
 
-bsnesexport bool sgb_init(bool version) {
+dllexport bool sgb_init(bool version) {
   return supergameboy.init(version);
 }
 
-bsnesexport void sgb_term() {
+dllexport void sgb_term() {
   supergameboy.term();
 }
 
-bsnesexport void sgb_power() {
+dllexport void sgb_power() {
   supergameboy.power();
 }
 
-bsnesexport void sgb_reset() {
+dllexport void sgb_reset() {
   supergameboy.reset();
 }
 
-bsnesexport void sgb_row(unsigned row) {
+dllexport void sgb_row(unsigned row) {
   supergameboy.row(row);
 }
 
-bsnesexport uint8_t sgb_read(uint16_t addr) {
+dllexport uint8_t sgb_read(uint16_t addr) {
   return supergameboy.read(addr);
 }
 
-bsnesexport void sgb_write(uint16_t addr, uint8_t data) {
+dllexport void sgb_write(uint16_t addr, uint8_t data) {
   supergameboy.write(addr, data);
 }
 
-bsnesexport unsigned sgb_run(uint32_t *samplebuffer, unsigned clocks) {
+dllexport unsigned sgb_run(uint32_t *samplebuffer, unsigned clocks) {
   return supergameboy.run(samplebuffer, clocks);
 }
 
-bsnesexport void sgb_save() {
+dllexport void sgb_save() {
   supergameboy.save();
 }
 
-bsnesexport void sgb_serialize(nall::serializer &s) {
+dllexport void sgb_serialize(nall::serializer &s) {
   supergameboy.serialize(s);
 }
